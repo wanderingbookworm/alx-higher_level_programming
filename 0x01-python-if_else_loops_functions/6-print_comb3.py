4c95df68d2716bc078bb899b1eff1e9220a1431e
estelle <oforiestelle@gmail.com>
2024-05-04 20:04:40 +0100

    Hopefully, this is the last correction for task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print('{}{}'.format(x, y), end='')
+        print('{:02d}'.format(x), end='')
+        print('{:02d}'.format(y), end='')
         if x != 8 and y != 9:
             print(', ', end='')
 print()
