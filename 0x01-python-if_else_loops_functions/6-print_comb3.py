84d1600ec6d7c07e3570ebd9eb8448b5dced3959
estelle <oforiestelle@gmail.com>
2024-05-04 17:19:46 +0100

    Adjusted the code to make sure that it prints out the correct output again

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(0, 10):
     for y in range(x + 1, 10):
         if x == 9 and y == 9:
-            print('89')
+            print('99')
 else:
     print('{}{}, '.format(x, y), end='')
