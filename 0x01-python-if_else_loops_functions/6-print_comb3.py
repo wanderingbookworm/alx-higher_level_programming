6972725fe8d18fb81703c5a37dbc03806e52539f
estelle <oforiestelle@gmail.com>
2024-05-04 20:13:45 +0100

    Giving up on task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print('{:02d}'.format(x), end='')
-        print('{:02d}'.format(y), end='')
+        print('{}{}'.format(x, y), end='')
         if x != 8 and y != 9:
             print(', ', end='')
 print()
